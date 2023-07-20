755a39664ef77a6691af30ab0dfd16084d1a9392
Nimesh9172 <nimeshdj123@gmail.com>
2023-07-20 12:08:32 +0530

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import {Fragment} from 'react'
+import classes from './Header.module.css'
+import mealsImage from '../../assets/bg1.jpg'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Swiggato</h1>
+        {/* <button>Cart</button> */}
+        <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="Delicious Foods"/>
+      </div>
+    </Fragment>
+  )
+}
+
+export default Header
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,44 @@
+import classes from './AvailableMeals.module.css'
+import Card from "../UI/Card";
+import MealItem from './MealItem/MealItem';
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 499.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 365.13,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 1290.00,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 999.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
